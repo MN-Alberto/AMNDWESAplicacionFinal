cc6566ce13ea944cc72194aac3ad32c4603149f1
Alberto Méndez <alberto.mennun@educa.jcyl.es>
2026-02-19 09:20:53 +0100

    docs: Se añaden comentarios al controlador y a las APIs
    docs: Se añaden comentarios al controlador y a las APIs

--- api/wsEliminarUsuario.php
+++ api/wsEliminarUsuario.php
@@ -12,10 +12,13 @@ require_once '../model/DBPDO.php';
 require_once '../model/Usuario.php';
 require_once '../model/UsuarioPDO.php';
 
+//variable del codUsuario a eliminar
 $codUsuario = $_REQUEST['codUsuario'] ?? "";
 
+//validamos el codigo y almacenamos el posible error en errorCodigo
 $errorCodigo= validacionFormularios::comprobarAlfaNumerico($codUsuario, 255, 0,0);
 
+//comprobamos si ha habido algún error, si lo hay devolvemos un json con el error correspondiente
 if($errorCodigo!=null){
     header('Content-Type: application/json');
     echo json_encode([
@@ -24,8 +27,10 @@ if($errorCodigo!=null){
     exit;
 }
 
+//en la variable usuario llamamos al método buscarUsuarioPorCodigo pasandole el codigo del usuario que se pasa como parametro
 $usuario= UsuarioPDO::buscarUsuarioPorCodigo($codUsuario);
 
+//si el usuario no contiene nada devolvemos un json con el error usuario no encontrado
 if(!$usuario){
     header('Content-Type: application/json');
     echo json_encode([
@@ -34,9 +39,10 @@ if(!$usuario){
     exit;
 }
 
+//llamamos al metodo eliminarUsuario pasandole el codUsuario a eliminar como parametro
 $resultado = UsuarioPDO::eliminarUsuario($codUsuario);
 
-
+//devolvemos un json con el codUsuario eliminado, todo OK y el mensaje de exito
 header('Content-Type: application/json');
 echo json_encode([
     "codigo" => $codUsuario,
--- controller/cCambiarPassword.php
+++ controller/cCambiarPassword.php
@@ -16,21 +16,24 @@ if(isset($_REQUEST['Cancelar'])){
 
 $entradaOK=true;
 
+//almacenamos en la variable el usuario que hay en la sesion
 $oUsuario=$_SESSION['userAMNDWESAplicacionFinal'];
 
+//variable para los errores de los campos
 $aErrores=[
     'password' => null,
     'nuevaPassword' => null,
     'repetirNuevaPassword' => null
 ];
 
-
+//si clicamos en aceptar
 if(isset($_REQUEST['Aceptar'])){
+    //validamos cada campo con la libreria
     $aErrores['password'] = validacionFormularios::comprobarAlfaNumerico($_REQUEST['passActual'],255,4,1);
     $aErrores['nuevaPassword'] = validacionFormularios::comprobarAlfaNumerico($_REQUEST['pass'],255,4,1);
     $aErrores['repetirNuevaPassword'] = validacionFormularios::comprobarAlfaNumerico($_REQUEST['pass2'],255,4,1);
 
-
+//comprobamos si ha habido algun error
     foreach ($aErrores as $valorCampo => $error) {
         if ($error != null) {
             $entradaOK = false;
@@ -38,22 +41,26 @@ if(isset($_REQUEST['Aceptar'])){
     }
 
 
+//si el cifrado de la contraseña actual introducida es diferente de la contraseña del usuario de la sesion almacenamos un error y cambiamos la entrada a false
     if (hash('sha256', $_REQUEST['passActual']) != $oUsuario->getPassword()) {
         $aErrores['password'] = "La contraseña actual no es correcta";
         $entradaOK = false;
     }
 
+//si la nueva contraseña 1 es diferente de la nueva contraseña 2 almacenamos un error y cambiamos a false
     if ($_REQUEST['pass'] != $_REQUEST['pass2']) {
         $aErrores['repetirNuevaPassword'] = "Las contraseñas nuevas no coinciden";
         $entradaOK = false;
     }
 
 
+//si tras todo lo anterior la entrada sigue OK
     if($entradaOK){
-
+        //llamamos al metodo cambiar password pasandole el usuario y la nueva contraseña
         $oUsuario = UsuarioPDO::cambiarPassword($oUsuario, $_REQUEST['pass']);
         $_SESSION['userAMNDWESAplicacionFinal'] = $oUsuario;
 
+        //volvemos a la pagina de editar
         $_SESSION['paginaAnterior'] = $_SESSION['paginaEnCurso'];
         $_SESSION['paginaEnCurso'] = "editar";
 
@@ -62,6 +69,6 @@ if(isset($_REQUEST['Aceptar'])){
     }
 }
 
-
+//llamamos a la vista
 require_once $view['Layout'];
 ?>
